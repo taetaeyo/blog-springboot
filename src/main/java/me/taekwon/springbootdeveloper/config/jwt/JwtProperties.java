24085773a21cd9e5baf565cbd2eb87bb24499ef2
taetaeyo <57551024+taetaeyo@users.noreply.github.com>
2023-10-27 17:36:06 +0900

    Chapter 9.2.4 finished

---
+++ src/main/java/me/taekwon/springbootdeveloper/config/jwt/JwtProperties.java
@@ -0,0 +1,16 @@
+package me.taekwon.springbootdeveloper.config.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties("jwt") // JAVA 클래스에 프로퍼티값을 가져와서 사용하는 어노테이션
+public class JwtProperties {
+    private String issuer;
+    private String secretKey;
+
+}
